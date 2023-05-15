fa8770f67244c64aa3d428203ae5634dfc4e234c
pradanadp <pradana.dimasdp@gmail.com>
2023-05-15 21:47:10 +0700

    Add feature for registering new user

--- controllers/user_controller.go
+++ controllers/user_controller.go
@@ -1,1 +1,33 @@
 package controllers
+
+import (
+	"alta/account-service-app/helpers"
+	"alta/account-service-app/models"
+	"database/sql"
+	"fmt"
+)
+
+func UserRegister(db *sql.DB, user models.User) (int64, error) {
+	sqlStatement := `
+	INSERT INTO users (
+		full_name, single_identity_number, birth_date, address, email, phone, password, balance
+	) VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+	`
+
+	passHashing := helpers.HashPass(user.Password)
+
+	result, err := db.Exec(
+		sqlStatement, user.FullName, user.SingleIdentityNumber, user.BirthDate, user.Address,
+		user.Email, user.PhoneNumber, passHashing, user.Balance,
+	)
+	if err != nil {
+		return 0, fmt.Errorf("add user: %v", err)
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, fmt.Errorf("add user: %v", err)
+	}
+
+	return id, nil
+}
