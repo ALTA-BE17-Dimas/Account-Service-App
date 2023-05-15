6ece577741b3836bafabdb5c8aa4547604b43293
pradanadp <pradana.dimasdp@gmail.com>
2023-05-15 23:48:35 +0700

    Update user model

--- models/user_model.go
+++ models/user_model.go
@@ -1,16 +1,13 @@
 package models
 
-import "time"
-
 type User struct {
 	BaseModel
 	FullName             string
 	SingleIdentityNumber string
-	BirthDate            time.Time
+	BirthDate            string
 	Address              string
 	Email                string
 	PhoneNumber          string
 	Password             string
 	Balance              float64
-	DeletedAt            time.Time
 }
