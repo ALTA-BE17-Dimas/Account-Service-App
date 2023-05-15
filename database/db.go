ee0fe0c716c5db7d5e9c59de084dc1dde0668a92
pradanadp <pradana.dimasdp@gmail.com>
2023-05-15 23:47:04 +0700

    Update database

--- database/db.go
+++ database/db.go
@@ -4,12 +4,14 @@ import (
 	"database/sql"
 	"fmt"
 	"time"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func DBConnect(connStr string) (*sql.DB, error) {
 	db, err := sql.Open("mysql", connStr)
 	if err != nil {
-		return nil, fmt.Errorf("error connecting to database: %w", err)
+		return nil, fmt.Errorf("[FAIL] error connecting to database: %w", err)
 	}
 
 	db.SetConnMaxLifetime(time.Minute * 3)
@@ -21,6 +23,7 @@ func DBConnect(connStr string) (*sql.DB, error) {
 		return nil, fmt.Errorf("error pinging database: %w", err)
 	}
 
-	fmt.Println("Database connected successfully")
+	fmt.Print("\n[OK] Database connected successfully.\n\n")
+
 	return db, nil
 }
