49d1ef56ddbc7b8cf0d581e5a1da47b3a32b9ba5
pradanadp <pradana.dimasdp@gmail.com>
2023-05-15 20:43:56 +0700

    Update db.go

--- database/db.go
+++ database/db.go
@@ -1,1 +1,26 @@
 package database
+
+import (
+	"database/sql"
+	"fmt"
+	"time"
+)
+
+func DBConnect(connStr string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", connStr)
+	if err != nil {
+		return nil, fmt.Errorf("error connecting to database: %w", err)
+	}
+
+	db.SetConnMaxLifetime(time.Minute * 3)
+	db.SetMaxOpenConns(10)
+	db.SetMaxIdleConns(10)
+
+	err = db.Ping()
+	if err != nil {
+		return nil, fmt.Errorf("error pinging database: %w", err)
+	}
+
+	fmt.Println("Database connected successfully")
+	return db, nil
+}
