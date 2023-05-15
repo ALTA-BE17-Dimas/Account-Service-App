10cb0f8310ef377d1f489e13d7f61f5f15819ce4
pradanadp <pradana.dimasdp@gmail.com>
2023-05-15 23:48:03 +0700

    Update user controller

--- controllers/user_controller.go
+++ controllers/user_controller.go
@@ -5,29 +5,51 @@ import (
 	"alta/account-service-app/models"
 	"database/sql"
 	"fmt"
+	"log"
 )
 
-func UserRegister(db *sql.DB, user models.User) (int64, error) {
+func UserRegister(db *sql.DB, user models.User) (string, error) {
 	sqlStatement := `
 	INSERT INTO users (
 		full_name, single_identity_number, birth_date, address, email, phone, password, balance
-	) VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+	) VALUES (?, ?, ?, ?, ?, ?, ?, ?);
 	`
 
-	passHashing := helpers.HashPass(user.Password)
+	// validating email format
+	if emailIsValid := helpers.ValidateEmail(user.Email); !emailIsValid {
+		log.Fatal("email format is invalid")
+	}
 
+	// validating birth date format
+	valid, birthDate, _ := helpers.ValidateDate(user.BirthDate)
+	if !valid {
+		log.Fatal("date is invalid. date format expected for input is (DD-MM-YYYY)")
+	}
+
+	// validating password
+	passIsValid := helpers.ValidatePassword(user.Password)
+	passHashing := ""
+	if passIsValid {
+		// hashing the password
+		passHashing = helpers.HashPass(user.Password)
+	} else {
+		log.Fatal("password should contain lowercase, uppercase, special character, and the length is more than 7")
+	}
+
+	// insert new data to database
 	result, err := db.Exec(
-		sqlStatement, user.FullName, user.SingleIdentityNumber, user.BirthDate, user.Address,
+		sqlStatement, user.FullName, user.SingleIdentityNumber, birthDate, user.Address,
 		user.Email, user.PhoneNumber, passHashing, user.Balance,
 	)
 	if err != nil {
-		return 0, fmt.Errorf("add user: %v", err)
+		return "", fmt.Errorf("add user: %v", err)
 	}
 
 	id, err := result.LastInsertId()
 	if err != nil {
-		return 0, fmt.Errorf("add user: %v", err)
+		return "", fmt.Errorf("add user: %v", err)
 	}
 
-	return id, nil
+	outputStr := fmt.Sprintf("User with ID %d registered successfully.", id)
+	return outputStr, nil
 }
